4ff848062fd97930788aaae7121b553ee91b5b03
Boghdan Pavlov <lukures@yandex.ru>
2016-05-24 15:31:46 +0300

    step 04 ng-repeat

--- build/app/app.js
+++ build/app/app.js
@@ -2,25 +2,26 @@
 var app = angular.module('app', [])
   //$scope содержит данные модели. Это связующее звено между контроллером и видом.
   //$scope всего лишь один из сервисов, внедренных в контроллер.
-app.controller('russiaCtrl', function russiaCtrl($scope) {
-  $scope.contry = {
-    name: 'Russia',
-    area: '606.400',
-    population: '42 625 583',
-    capital: {
-      name : 'Moskov'
-    }
-  }
+app.controller('contriesCtrl', function contriesCtrl($scope) {
+
+  $scope.contries = [{
+      name: 'Russia',
+      area: '603 628',
+      population: '42 825 883',
+      capital: {
+          name: 'Kiev'
+      }
+  },
+  {
+      name: 'Canada',
+      area: '9 984 670',
+      population: '34 568 211',
+      capital: {
+          name: 'Ottava'
+      }
+  }];
+
 })
-app.controller('canadaCtrl', function canadaCtrl($scope) {
-  $scope.contry = {
-    name: 'Canada',
-    area: '606.400',
-    population: '42 625 583',
-    capital: {
-      name : 'Moskov'
-    }
-  }
-})
+
 
 
