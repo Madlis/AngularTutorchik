8cf351bb7c5b6363b269f147c0d12a7e58d7b93e
Boghdan Pavlov <lukures@yandex.ru>
2016-05-24 15:16:06 +0300

    step 03 Milti controllers

--- src/app/app.js
+++ src/app/app.js
@@ -2,8 +2,7 @@
 var app = angular.module('app', [])
   //$scope содержит данные модели. Это связующее звено между контроллером и видом.
   //$scope всего лишь один из сервисов, внедренных в контроллер.
-app.controller('contryCtrl',contryCtrl)
-function contryCtrl($scope) {
+app.controller('russiaCtrl', function russiaCtrl($scope) {
   $scope.contry = {
     name: 'Russia',
     area: '606.400',
@@ -12,4 +11,15 @@ function contryCtrl($scope) {
       name : 'Moskov'
     }
   }
-}
+})
+app.controller('canadaCtrl', function canadaCtrl($scope) {
+  $scope.contry = {
+    name: 'Canada',
+    area: '606.400',
+    population: '42 625 583',
+    capital: {
+      name : 'Moskov'
+    }
+  }
+})
+
