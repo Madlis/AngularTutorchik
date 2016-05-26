2677148d059d58d6642b30ddf494b8d21b70312d
Boghdan Pavlov <lukures@yandex.ru>
2016-05-26 14:18:37 +0300

    filter

--- src/app/app.js
+++ src/app/app.js
@@ -3,17 +3,18 @@ var app = angular.module('app', [])
   //$scope содержит данные модели. Это связующее звено между контроллером и видом.
   //$scope всего лишь один из сервисов, внедренных в контроллер.
 app.controller('ListCtrl', function ListCtrl($scope) {
-  $scope.dataValue = false;
-})
+  $scope.tasks = [
+          { action: "Buy flowers", complete: false },
+          { action: "Go to gym", complete: false },
+          { action: "Buy snickers", complete: true },
+          { action: "Buy notebook", complete: false },
+          { action: "Call friends", complete: false },
+          { action: "Go to cinema", complete: false },
+          { action: "Buy Big-Mac", complete: true },
+          { action: "Buy a book", complete: false },
+          { action: "Call mom", complete: false }
+      ];
+  });
 
-
-// apply - он позволяет выполнить какую-то функцию на объекте
-// var a = { name: 'qwe'};
-
-// function test () {
-//   alert(this.name);
-// }
-// test(); - приведет к ошибке в виндов нету
-// test.apply(a, name) - название оъекта к которому хотим применить и аргумент
 
 
