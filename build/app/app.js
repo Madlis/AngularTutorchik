35385f8504ed1adff141d04dda1f8d8af5807339
Boghdan Pavlov <lukures@yandex.ru>
2016-05-27 00:11:51 +0300

    Form_directive_attributes

--- build/app/app.js
+++ build/app/app.js
@@ -3,31 +3,19 @@ var app = angular.module('app', [])
   //$scope содержит данные модели. Это связующее звено между контроллером и видом.
   //$scope всего лишь один из сервисов, внедренных в контроллер.
 app.controller('ListCtrl', function ListCtrl($scope) {
-  $scope.addNewUser = function (userDetails) {
-    if (myForm.$valid) {
-      $scope.message = "Name: " + userDetails.name + " Email: " + userDetails.email;
-    } else {
-      $scope.showValidation = true;
-    }
-  };
-  $scope.message = "Ready";
-  $scope.getError = function (error) {
-      if (angular.isDefined(error)) {
-          //объект который здесь ожидается получить - error но он будет сформирован только тогда когда будет допущена хотя бы одна ошибка валидации,
-          //а пока их нет он undefined и для того чтобы избежать ошибки нужно использовать проверку
-          if (error.required) {
-              return "Please enter a value";
-          } else if (error.email) {
-              return "Please enter a valid email address";
-          }
-      }
-  };
-
-//в данном примере для упрощения реализации вспомогательных сообщений об ошибках логика формирования текста ошибки
-//вынесена в отдельный метод так как для многих элементов управления текст ошибки будет похож то
-//нет смысла дублировать его в каждом элементе разметки
+  $scope.requireValue = true;
+  $scope.matchPattern = new RegExp("^[a-z]");
 });
 // isDefined - Определяет, является ли значение определенным.
+  // ng-model="string"
+  // [name="string"]
+  // [required="string"]
+  // [ng-required="boolean"]
+  // [ng-minlength="number"]
+  // [ng-maxlength="number"]
+  // [ng-pattern="string"]
+  // [ng-change="string"]
+  // [ng-trim="boolean"]>
 
 
 
