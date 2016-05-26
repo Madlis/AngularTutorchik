16129686260045011f1b9d6f664ffb94a4809ab0
Boghdan Pavlov <lukures@yandex.ru>
2016-05-26 23:46:26 +0300

    valid if {error function}

--- build/app/app.js
+++ build/app/app.js
@@ -5,10 +5,25 @@ var app = angular.module('app', [])
 app.controller('ListCtrl', function ListCtrl($scope) {
   $scope.addNewUser = function (userDetails) {
         $scope.message = "Name: " + userDetails.name + " Email: " + userDetails.email;
-  }
+  };
   $scope.message = "Ready";
-});
+  $scope.getError = function (error) {
+      if (angular.isDefined(error)) {
+          //объект который здесь ожидается получить - error но он будет сформирован только тогда когда будет допущена хотя бы одна ошибка валидации,
+          //а пока их нет он undefined и для того чтобы избежать ошибки нужно использовать проверку
+          if (error.required) {
+              return "Please enter a value";
+          } else if (error.email) {
+              return "Please enter a valid email address";
+          }
+      }
+  };
 
+//в данном примере для упрощения реализации вспомогательных сообщений об ошибках логика формирования текста ошибки
+//вынесена в отдельный метод так как для многих элементов управления текст ошибки будет похож то
+//нет смысла дублировать его в каждом элементе разметки
+});
+// isDefined - Определяет, является ли значение определенным.
 
 
 
